b7423573435a021626efd4240bb15da1475861b7
Jawad <96871883+PJmax26@users.noreply.github.com>
2024-02-12 22:10:59 +0000

    new project commit
    added new project, GO2DEN...

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -7,11 +7,12 @@ const Skills = () => {
         My Programming <span className="text-orange">Skills</span>
       </h3>
       <h1 className="hlg text-center">Tools I Work With</h1>
-      <div className="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-7 gap-3 p-5">
+      <div className="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-7 gap-1 sm:gap-3 p-5">
         {skillsList.map(({ id, title, img }) => (
           <div key={id} className="p-3 rounded-md bg-indigo_dark  text-center">
             <img src={img} alt="" />
-            <p>{title}</p>
+            {/* <p className="hsm">{title}</p> */}
+            <small>{title}</small>
           </div>
         ))}
       </div>
